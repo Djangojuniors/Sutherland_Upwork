e8e2634460f4bf349cc555d308f468ab01b72a68
Djangojuniors <djnagojuniors@gmail.com>
2021-10-23 22:58:39 +0530

    Tapas

--- Extraction.py
+++ Extraction.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pdfminer.pdfdocument
 import pdfplumber
-
-# import camelot
-# import tabula
-import streamlit as st
+import camelot
+import tabula
+# import streamlit as st
 import csv
+from predict_table import detect_tables
 
 KEY_WORDS = ['CONSOLIDATED RESULTS OF OPERATIONS', 'Income Statement', 'INCOME STATEMENTS',
              'CONSOLIDATED STATEMENTS OF OPERATIONS',
@@ -16,24 +16,24 @@ KEY_WORDS = ['CONSOLIDATED RESULTS OF OPERATIONS', 'Income Statement', 'INCOME S
 table_heading = []
 # uploaded_file = st.file_uploader("Choose a file", "pdf")
 
-filename = r'F:\Upwork\Perdita_Upwork\10 K Docs\Apple.pdf'
+filename = r'10 K Docs\CocaCola.pdf'
 
 with pdfplumber.open(filename) as _pdf:
+    numd = 0
     for pages in _pdf.pages:
         _text = pages.extract_text()
         # print(_text)
 
         for d in KEY_WORDS:
             if d in _text:
-                table_heading.append(d)
-                labels_in_pages = pages.extract_text()
-                # print(pages, labels_in_pages)
-                # print(type(pages))
+                print(d)
+                print(numd)
+                detect_tables(pdf_file=filename, pg=numd + 1)
+
+                # table_heading.append(d)
+                # labels_in_pages = pages.extract_text()
+                print(pages)
                 # st.write(labels_in_pages)
-                with open('invoice.csv', 'w') as invoice:
-                    writer = csv.writer(invoice)
-                    writer.writerow(labels_in_pages)
-
-
+        numd+=1
 
 
